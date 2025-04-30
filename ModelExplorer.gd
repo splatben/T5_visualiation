6300bf3dc520116946d50d4f0f3e76f15da9a5f8
Collot <benoit.collot@etudiant.univ-reims.fr>
2025-04-30 14:51:54 +0200

    chargement en parrallèle

--- ModelExplorer.gd
+++ ModelExplorer.gd
@@ -3,8 +3,8 @@ extends FileDialog
 signal is_loaded(gltf:Node)
 signal load_failed()
 	
-
-var gltf_doc = GLTFDocument.new()
+var _thread:Thread
+var _gltf_doc = GLTFDocument.new()
 
 func _ready():
 	self.file_selected.connect(_on_file_selected)
@@ -12,31 +12,49 @@ func _ready():
 func _on_file_selected(file:String):
 	var ext = file.get_extension()
 	if ext == "glb" or ext == "gltf":
-		load_gltf(file)
+		if(_thread):
+			if(_thread.is_alive()):
+				return;
+			else:
+				_thread.wait_to_finish()
+		_thread = Thread.new()
+		_thread.start(Callable(self, "_load_gltf").bind(file))
 	elif ext == "tscn" :
-		var nodePacked = load(file) #charger une scène qui n'est pas deja dans la scène 
-		var node = nodePacked.instantiate()
-		is_loaded.emit(node)
+		if(_thread):
+			if(_thread.is_alive()):
+				return;
+			else:
+				_thread.wait_to_finish()
+		_thread = Thread.new()
+		_thread.start(Callable(self, "_load_gltf").bind(file))
 	hide()
 
-func load_gltf(file:String):
+func _load(file:String):
+	var nodePacked = load(file) #charger une scène qui n'est pas deja dans la scène 
+	var node = nodePacked.instantiate()
+	_emit_load.call_deferred(node)
+	
+func _load_gltf(file:String):
 	var gltf_state: GLTFState = GLTFState.new()
 	gltf_state.handle_binary_image = GLTFState.HANDLE_BINARY_EMBED_AS_UNCOMPRESSED
 	var err = ERR_FILE_CANT_OPEN
 
-	err = gltf_doc.append_from_file(file, gltf_state)
+	err = _gltf_doc.append_from_file(file, gltf_state)
 	
 	var gltf:Node = null
 	
 	if err == OK:
-		gltf = gltf_doc.generate_scene(gltf_state)
-		_emit_gltf_load.call_deferred(gltf)
+		gltf = _gltf_doc.generate_scene(gltf_state)
+		_emit_load.call_deferred(gltf)
 	else:
-		_emit_gltf_load_failed.call_deferred()
+		_emit_load_failed.call_deferred()
 
-func _emit_gltf_load(gltf) -> void:
+func _emit_load(gltf) -> void:
 	is_loaded.emit(gltf)
 	
-func _emit_gltf_load_failed() -> void:
+func _emit_load_failed() -> void:
 	print("echec")
 	load_failed.emit()
+	
+func _exit_tree():
+	_thread.wait_to_finish()
--- worker.gd
+++ worker.gd
@@ -6,7 +6,9 @@ var _callable:Callable
 func _init(cb:Callable):
 	_callable = cb
 
-func start() -> Variant:
+func start():
 	_thread = Thread.new()
 	var _discard = _thread.start(_callable)
-	return _thread.wait_to_finish()
+	
+func _exit_tree():
+	_thread.wait_to_finish()
